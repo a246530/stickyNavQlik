232af0ebcc2ccfc434ed90624634bb3ca4c1a6f4
carsonm <a246530@gmail.com>
2016-02-12 17:07:50 -0500

    Initial Release + image

---
+++ stickyNavQlik/script.js
@@ -0,0 +1,15 @@
+var template_path = Qva.Remote + "?public=only&name=Extensions/stickyNavQlik/";
+Qva.LoadCSS(template_path + "adjustDefault.css");
+function extension_Done(){
+	Qv.AddExtension('stickyNavQlik', function() {
+		var doc = Qv.GetCurrentDocument();
+		var _this = this;
+		var html="";
+	});
+}; 
+
+
+
+
+
+
